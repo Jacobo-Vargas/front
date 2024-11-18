87b17f7e3697a0c13619a96b12752a7209bdada4
Jacobo Vargas <jacobo.vargas@cofincafe.com>
2024-11-17 22:08:57 -0500

    crud cliente terminado, falta filtro

--- src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { AppComponent } from './home/app.component';
-import { CrudClientComponent } from './crud-client/crud-client.component';
-
-export const routes: Routes = [
-    { path: 'app-crud-client', component: CrudClientComponent}
-];
