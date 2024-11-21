3b2cad87ffa84ed7c0b88290f22267feea2bc723
Jacobo Vargas <jacobo.vargas@cofincafe.com>
2024-11-20 22:29:00 -0500

    advance in front of cruds

--- src/app/modules/crud-cuenta/crud-cuenta.component.ts
+++ src/app/modules/crud-cuenta/crud-cuenta.component.ts
@@ -11,7 +11,6 @@ import { CRUDService } from '../../services/crud.service';
 export class CrudCuentaComponent {
 
   dataAccounts: any[] = [];  // Lista de clientes
-  sucursales: any[] = [];   // Lista de sucursales cargadas desde el endpoint
   clientes: any[] = []; // Lista de tipos de clientes
   productos: any[] = [];
   accountData: any = {};     // Datos del cuenta (para crear o editar)
@@ -31,6 +30,15 @@ export class CrudCuentaComponent {
     this.getCuentas();
     this.getClientes();
     this.getProductos();
+  }
+
+  deleteCuenta(cuentaId: number): void {
+    this.crudService.deleteById(this.endPoint + this.endpointDelete, cuentaId).then(() => {
+      this.getCuentas();
+      //this.dataClients = this.dataClients.filter((item: any)  => item.id !== clientId);
+    }).catch(error => {
+      console.error('Error al eliminar el cliente:', error);
+    });
   }
 
   getCuentas() {
@@ -55,6 +63,59 @@ export class CrudCuentaComponent {
     }).catch(error => {
       console.error('Error al obtener los datos:', error);
     });
+  }
+
+  async getByNumeroCuenta(numeroCuenta:string) {
+    this.accountData = {};
+    this.crudService.dataFormAux = null;
+    this.isEditing = true;
+    this.crudService.getByArgument(this.endPoint +'/numero/', numeroCuenta).then(async () => {
+      await this.crudService.delay(100);
+    }).catch(error => {
+      console.error('Error al obtener los datos:', error);
+    });
+
+    await this.crudService.delay(1000);
+    if (this.crudService.dataFormAux){
+      this.editAccount(this.crudService.dataFormAux);
+    } 
+  }
+
+  editAccount(account: any): void {
+    this.isEditing = true;
+    this.accountData = { ...account };
+  }
+
+  isFormValid(): boolean {
+    return (
+      this.accountData.numeroCuenta &&
+      this.accountData.productoId
+    ) ? true : false;
+  }
+
+  resetForm() {
+    this.accountData = {};
+    this.isEditing = false;
+  }
+
+   // Función de envío del formulario (guardar o actualizar)
+   onSubmit() {
+    if (this.isEditing) {
+      // Lógica para actualizar el cliente
+      this.crudService.update(this.endPoint + '/'+this.accountData.id, this.accountData).then(() => {
+        this.getCuentas();
+        this.resetForm();
+      });
+    } else {
+      if (this.accountData) {
+        this.crudService.create(this.endPoint, this.accountData).then(() => {
+          this.getCuentas();
+          this.resetForm();
+        });
+      } else {
+        this.alertService.createAlert("Ingrese los datos", 'error', false);
+      }
+    }
   }
 
 }
--- src/app/modules/crud-sucursal/crud-sucursal.component.ts
+++ src/app/modules/crud-sucursal/crud-sucursal.component.ts
@@ -14,17 +14,93 @@ export class CrudSucursalComponent {
   dataSucursales: any[] = [];  // Lista de clientes
   sucursales: any[] = [];   // Lista de sucursales cargadas desde el endpoint
   tipoClientes: any[] = []; // Lista de tipos de clientes
-  clientData: any = {};     // Datos del cliente (para crear o editar)
+  sucursalData: any = {};     // Datos del cliente (para crear o editar)
   isEditing: boolean = false; // Determina si estamos editando o creando
 
   endPoint = '/sucursal';
   endpointDelete = '/delete';
-  password = 'sucursal';
+  password = 'sucursal'; 
 
   constructor(public translate: TranslateService,
     private alertService: AlertService,
     private crudService: CRUDService) {
 
+  }
+
+  ngOnInit(): void {
+    this.getSucursales();
+  }
+
+  deleteSucursal(cuentaId: number): void {
+    this.crudService.deleteById(this.endPoint + this.endpointDelete, cuentaId).then(() => {
+      this.getSucursales();
+      //this.dataClients = this.dataClients.filter((item: any)  => item.id !== clientId);
+    }).catch(error => {
+      console.error('Error al eliminar el cliente:', error);
+    });
+  }
+
+  getSucursales() {
+    this.crudService.getData(this.endPoint).then(() => {
+      this.dataSucursales = this.crudService.dataList;
+    }).catch(error => {
+      console.error('Error al obtener los datos:', error);
+    });
+  }
+
+  async getByNombreSucursal(nombre:string) {
+    this.sucursalData = {};
+    this.crudService.dataFormAux = null;
+    this.isEditing = true;
+    this.crudService.getByArgument(this.endPoint +'/nombre/', nombre).then(async () => {
+      await this.crudService.delay(100);
+    }).catch(error => {
+      console.error('Error al obtener los datos:', error);
+    });
+
+    await this.crudService.delay(1000);
+    if (this.crudService.dataFormAux){
+      this.editSucursal(this.crudService.dataFormAux);
+    } 
+  }
+
+  editSucursal(account: any): void {
+    this.isEditing = true;
+    this.sucursalData = { ...account };
+  }
+
+  isFormValid(): boolean {
+    return (
+      this.sucursalData.nombre &&
+      this.sucursalData.direccion &&
+      this.sucursalData.telefono &&
+      this.sucursalData.ciudad
+    ) ? true : false;
+  }
+
+  resetForm() {
+    this.sucursalData = {};
+    this.isEditing = false;
+  }
+
+   // Función de envío del formulario (guardar o actualizar)
+   onSubmit() {
+    if (this.isEditing) {
+      // Lógica para actualizar el cliente
+      this.crudService.update(this.endPoint + '/'+this.sucursalData.id, this.sucursalData).then(() => {
+        this.getSucursales();
+        this.resetForm();
+      });
+    } else {
+      if (this.sucursalData) {
+        this.crudService.create(this.endPoint, this.sucursalData).then(() => {
+          this.getSucursales();
+          this.resetForm();
+        });
+      } else {
+        this.alertService.createAlert("Ingrese los datos", 'error', false);
+      }
+    }
   }
 
 }
